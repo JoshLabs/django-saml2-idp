a95f92271a1b14ccdaee4c8ac43bf04a841aa37a
arpit jain <arpit.jain@joshtechnologygroup.com>
2015-02-27 17:06:49 +0530

    Added allowed hosts

--- idptest/settings/base.py
+++ idptest/settings/base.py
@@ -43,7 +43,7 @@ TIME_ZONE = 'America/Chicago'
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
-
+ALLOWED_HOSTS = ['*']
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
